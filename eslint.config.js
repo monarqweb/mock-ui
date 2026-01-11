d594606313550e14502ad750078bd4523fd89076
JacobRex <jacobrex@gmail.com>
2026-01-11 03:45:28 -0600

    fix(ci): add missing scripts and fix linting errors

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules', 'docs/**'] },
+  { ignores: ['dist', 'node_modules', 'docs/**', 'vite.config.ts'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -272,7 +272,6 @@ function ComboboxChip({
 
 function ComboboxChipsInput({
   className,
-  children,
   ...props
 }: ComboboxPrimitive.Input.Props) {
   return (
